613eaa6ea0999088c1cda4c635715dbea767fcab
Marko Samastur <markos@gaivo.net>
2017-01-20 19:38:04 +0100

    Start of table processing script

---
+++ tables.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+'''
+SpanishDict tables:
+
+1. Indicative:
+    1. Present
+    2. Preterite
+    3. Imperfect
+    4. Conditional
+    5. Future
+
+2. Subjunctive:
+    1. Present
+    2. Imperfect
+    3. Imperfect 2
+    4. Future
+
+3. Imperative:
+    1. Imperative
+
+4. Perfect:
+    1. Present
+    2. Preterite
+    3. Past
+    4. Conditional
+    5. Future
+
+5. Perfect Subjunctive:
+    1. Present
+    2. Past
+    3. Future
+
+CSS selector: .vtable:nth-child(table_index) td:nth-child(column_index)
+table_index and column_index start with 1.
+'''
